2fb8f33776b6288753be54eed50325a89953c6d6
konradasb <konradasb0@gmail.com>
2023-11-04 14:05:32 +0200

    test: Add tests for scheduler's status filter

--- server/scheduler/filter/status/status.go
+++ server/scheduler/filter/status/status.go
@@ -8,19 +8,23 @@ import (
 	"github.com/hostinger/fireactions/server/scheduler/filter"
 )
 
+// Filter implements filter.Filter interface.
 type Filter struct {
 }
 
+// New creates new Filter.
 func New() *Filter {
 	return &Filter{}
 }
 
 var _ filter.Filter = &Filter{}
 
+// Name returns the name of the filter.
 func (f *Filter) Name() string {
 	return "status"
 }
 
+// Filter filters out nodes that are not in fireactions.NodeStatusReady state.
 func (f *Filter) Filter(ctx context.Context, runner *fireactions.Runner, node *fireactions.Node) (bool, error) {
 	if node.Status != fireactions.NodeStatusReady {
 		return false, fmt.Errorf("node is not ready: status %s", node.Status)
---
+++ server/scheduler/filter/status/status_test.go
@@ -0,0 +1,59 @@
+package status
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hostinger/fireactions"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFiler(t *testing.T) {
+	type args struct {
+		ctx    context.Context
+		runner *fireactions.Runner
+		node   *fireactions.Node
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "returns true when node is ready",
+			args: args{
+				ctx:    context.Background(),
+				runner: &fireactions.Runner{},
+				node:   &fireactions.Node{Status: fireactions.NodeStatusReady},
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "returns false when node is not ready",
+			args: args{
+				ctx:    context.Background(),
+				runner: &fireactions.Runner{},
+				node:   &fireactions.Node{Status: fireactions.NodeStatusCordoned},
+			},
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := New()
+			got, err := f.Filter(tt.args.ctx, tt.args.runner, tt.args.node)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Filter.Filter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Filter.Filter() = %v, want %v", got, tt.want)
+			}
+
+			assert.Equal(t, f.Name(), "status")
+		})
+	}
+}
