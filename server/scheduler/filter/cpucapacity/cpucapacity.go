ffdaf2bf9d2f29f8957126c60ffd6ab597e237cd
konradasb <konradasb0@gmail.com>
2023-11-04 14:16:55 +0200

    test: Add tests for scheduler's cpucapacity filter

--- server/scheduler/filter/cpucapacity/cpucapacity.go
+++ server/scheduler/filter/cpucapacity/cpucapacity.go
@@ -8,9 +8,11 @@ import (
 	"github.com/hostinger/fireactions/server/scheduler/filter"
 )
 
+// Filter implements the filter.Filter interface.
 type Filter struct {
 }
 
+// New creates a new Filter.
 func New() *Filter {
 	return &Filter{}
 }
@@ -22,6 +24,7 @@ func (f *Filter) Name() string {
 	return "cpu-capacity"
 }
 
+// Filter filters nodes based on available CPU capacity.
 func (f *Filter) Filter(ctx context.Context, runner *fireactions.Runner, node *fireactions.Node) (bool, error) {
 	if !node.CPU.IsAvailable(runner.Resources.VCPUs) {
 		return false, fmt.Errorf("node doesn't have enough CPU capacity: requested %d, available %d", runner.Resources.VCPUs, node.CPU.Available())
