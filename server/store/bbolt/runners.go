58f725d093b2de4e7a55857d01ead9aba4667278
konradasb <konradasb0@gmail.com>
2023-11-29 12:30:39 +0200

    chore: Remove leftover function

--- server/store/bbolt/runners.go
+++ server/store/bbolt/runners.go
@@ -3,7 +3,6 @@ package bbolt
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/hostinger/fireactions"
@@ -92,73 +91,6 @@ func (s *Store) SaveRunner(ctx context.Context, runner *fireactions.Runner) erro
 	}
 
 	return nil
-}
-
-func (s *Store) AllocateRunner(ctx context.Context, nodeID, runnerID string) (*fireactions.Node, error) {
-	node := &fireactions.Node{}
-	err := s.db.Update(func(tx *bbolt.Tx) error {
-		nodesBucket := tx.Bucket([]byte("nodes"))
-
-		v := nodesBucket.Get([]byte(nodeID))
-		if v == nil {
-			return store.ErrNotFound
-		}
-
-		err := json.Unmarshal(v, node)
-		if err != nil {
-			return err
-		}
-
-		runnersBucket := tx.Bucket([]byte("runners"))
-		runnerBucket := runnersBucket.Bucket([]byte(runnerID))
-		if runnerBucket == nil {
-			return store.ErrNotFound
-		}
-
-		v = runnerBucket.Get([]byte("runner"))
-		if v == nil {
-			return store.ErrNotFound
-		}
-
-		runner := &fireactions.Runner{}
-		err = json.Unmarshal(v, runner)
-		if err != nil {
-			return err
-		}
-
-		if runner.NodeID != nil {
-			return fmt.Errorf("runner %s is already allocated to node %s", runner.ID, *runner.NodeID)
-		}
-
-		runner.NodeID = &node.ID
-		runner.UpdatedAt = time.Now()
-
-		node.CPU.Reserve(runner.Resources.VCPUs)
-		node.RAM.Reserve(runner.Resources.MemoryMB * 1024 * 1024)
-		node.UpdatedAt = time.Now()
-
-		data, err := json.Marshal(runner)
-		if err != nil {
-			return err
-		}
-
-		err = runnerBucket.Put([]byte("runner"), data)
-		if err != nil {
-			return err
-		}
-
-		data, err = json.Marshal(node)
-		if err != nil {
-			return err
-		}
-
-		return nodesBucket.Put([]byte(node.ID), data)
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return node, nil
 }
 
 func (s *Store) UpdateRunnerWithTransaction(ctx context.Context, txn store.Tx, id string, runnerUpdateFn func(*fireactions.Runner) error) (*fireactions.Runner, error) {
